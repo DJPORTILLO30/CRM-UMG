3f5be5723c7bb79a5b7cc80923fb078d10c79d9e
Luis Juárez <luisenriquejuarez17@gmail.com>
2022-09-19 23:25:06 -0600

    Creación de controladores contactos
    Se crearon los controladores para contactos

---
+++ controllers/contactos/contactos.js
@@ -0,0 +1,30 @@
+
+const getItems = (req, res) => {
+
+    res.send({mensaje: "contactos"});
+
+}
+
+const getItem = (req, res) => {
+
+    res.send({mensaje: "contactos"});
+
+}
+
+const createItem = (req, res) => {
+
+    res.send({mensaje: "contactos"});
+
+}
+
+const updateItem = (req, res) => {
+
+    res.send({mensaje: "contactos"});
+
+}
+
+const deleteItem = (req, res) => {
+
+    res.send({mensaje: "contactos"});
+
+}
