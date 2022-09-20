6fbbf9cd69bc9fca301b7a2a2218c8311596cc89
Luis Juárez <luisenriquejuarez17@gmail.com>
2022-09-20 00:00:31 -0600

    Creación controladores campañas
    Creación de controladores para campañas

---
+++ controllers/campañas/campañas.js
@@ -0,0 +1,33 @@
+
+
+const getItems = (req, res) => {
+
+    res.send({mensaje: "campañas"});
+
+}
+
+const getItem = (req, res) => {
+
+    res.send({mensaje: "campañas"});
+
+}
+
+const createItem = (req, res) => {
+
+    res.send({mensaje: "campañas"});
+
+}
+
+const updateItem = (req, res) => {
+
+    res.send({mensaje: "campañas"});
+
+}
+
+const deleteItem = (req, res) => {
+
+    res.send({mensaje: "campañas"});
+
+}
+
+module.exports ={getItems, getItem, createItem, updateItem, deleteItem}
