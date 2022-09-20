04a548249d68146ebc06c41355f0c771343f01ff
Endercarias <endercarias@gmail.com>
2022-09-20 00:06:14 -0600

    Creación de las rutas de campañas
    Creación de las rutas de campañas

---
+++ routes/campañas.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const {getItems, getItem, createItem, updateItem, deleteItem} = require("../controllers/campañas")
+
+router.get("/", getItems);
+router.get("/", getItem);
+router.post("/", createItem);
+router.put("/", updateItem);
+router.delete("/", deleteItem);
+
+module.exports = router
