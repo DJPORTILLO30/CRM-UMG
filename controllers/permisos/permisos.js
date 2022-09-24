5b0b3e272338d62337c3d1e41d4f08f65a4848a6
Yessenia Mateo <yesselimateo@gmail.com>
2022-09-23 22:34:06 -0600

    Cambio naming del código

--- controllers/permisos/permisos.js
+++ controllers/permisos/permisos.js
@@ -3,38 +3,38 @@
 /**
  * este es el controlador encargado de mostrar la lista de permisos
  */
-const mostrarPermisos = (req, res) =>{
+const getPermissions = (req, res) =>{
     res.send({mensaje: "permisos"});
 }
 
 /**
  * este es el controlador encargado de mostrar el detalle de un permiso
  */
- const mostrarPermiso = (req, res) =>{
+ const getPermission= (req, res) =>{
     res.send({mensaje: "permisos"});
 }
 
 /**
  * este es el controlador encargado de insertar un permiso
  */
- const insertarPermiso = (req, res) =>{
+ const createPermission = (req, res) =>{
     res.send({mensaje: "permisos"});
 }
 
 /**
  * este es el controlador encargado de modificar un permiso
  */
- const modificarPermiso = (req, res) =>{
+ const updatePermission = (req, res) =>{
     res.send({mensaje: "permisos"});
 }
 
 /**
  * este es el controlador encargado de eliminar un permiso
  */
- const eliminarPermiso = (req, res) =>{
+ const deletePermission = (req, res) =>{
     res.send({mensaje: "permisos"});
 }
 
 
-module.exports={mostrarPermisos, mostrarPermiso, insertarPermiso, modificarPermiso,eliminarPermiso}
+module.exports={getPermission, getPermissions, createPermission, updatePermission, deletePermission}
 
