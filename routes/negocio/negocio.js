7063030a869bc6c021ed9d9cc461f1fb78f6e7ed
AngelMCV <angelmcabrera01@gmail.com>
2022-09-25 20:19:59 -0600

    Creacion de controladores y rutas en feature-Negocios

---
+++ routes/negocio/negocio.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router= express.Router();
+const {
+
+     getBusis, 
+     getBusi,
+     createBusi, 
+     updateBusi, 
+     deleteBusi
+      
+} = require("../../controllers/negocio/negocio");
+
+/*Mostrar lista de negocios*/
+router.get("/", getBusis);
+
+/*Mostrar detalles*/
+router.get("/:id", getBusi);
+
+/*Crear un negocio*/
+router.post("/", createBusi);
+
+/*Actualiza un negocio*/
+router.put("/", updateBusi);
+
+/*Elimina un negocio*/
+router.delete("/", deleteBusi);
+
+
+module.exports = router;
