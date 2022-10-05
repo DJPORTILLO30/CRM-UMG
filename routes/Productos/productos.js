9601696241c17e9531bd37af89d45248342b0ff7
PDHElite <60772764+PDHElite@users.noreply.github.com>
2022-10-04 21:39:26 -0600

    Arreglo de ruta de la categoria Productos.

--- routes/Productos/productos.js
+++ routes/Productos/productos.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { getItems, getItem, createItem, updateItem, deleteItem } = require("../controllers/productos");
+const { getProducts, getProduct, insertProducts, updateProducts,deleteProducts } = require("..controllers/Productos/productos.js");
 
 
-router.get("/", getItems);
-router.get("/", getItem);
-router.post("/", createItem);
-router.put("/", updateItem);
-router.delete("/", deleteItem);
+router.get("/", getProducts);
+router.get("/", getProduct);
+router.post("/", insertProducts);
+router.put("/", updateProducts);
+router.delete("/", deleteProducts);
 
 module.exports = router
