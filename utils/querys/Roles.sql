96c2157b35bcac772d0867f0b00baf1984191cc7
lighnio <charlesvas16@outlook.com>
2022-10-19 12:50:05 -0600

    modificado el script sql quitando la collation

--- utils/querys/Roles.sql
+++ utils/querys/Roles.sql
@@ -5,4 +5,4 @@ CREATE TABLE roles (
   createdAt datetime DEFAULT NULL,
   updatedAt datetime DEFAULT NULL,
   PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
